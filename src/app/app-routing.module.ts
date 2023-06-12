bd99dbc28f5ee04d9ee500499b1c5635f654c3bd
Niteshsingh1 <niteshsingh5121@gmail.com>
2023-06-12 19:32:09 +0530

    assignments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CatsComponent } from './cats/cats.component';
+import { DogComponent } from './dog/dog.component';
+import { MiceComponent } from './mice/mice.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:"cats", component:CatsComponent},
+{path:"dog", component:DogComponent},
+{path:"mice", component:MiceComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CatsComponent } from './cats/cats.component';
+import { DogComponent } from './dog/dog.component';
+import { MiceComponent } from './mice/mice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatsComponent,
+    DogComponent,
+    MiceComponent
   ],
   imports: [
     BrowserModule,
